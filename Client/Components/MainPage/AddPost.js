a39ab6162ebf4cddc47f9f9017dc2069630586a8
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-09-11 23:01:00 +0500

    Add Post Control Added

--- Client/Components/MainPage/AddPost.js
+++ Client/Components/MainPage/AddPost.js
@@ -5,9 +5,13 @@ const AddPost = () => {
   return (
     <div className="flex flex-col bg-white p-2 rounded-md space-y-3">
       <div className="Top text-xs flex items-center bg-white space-x-2">
-      <Avatar src={"https://www.esafety.gov.au/sites/default/files/2019-08/Remove%20images%20and%20video.jpg"} />
+        <Avatar
+          src={
+            "https://www.esafety.gov.au/sites/default/files/2019-08/Remove%20images%20and%20video.jpg"
+          }
+        />
         <input
-          className="w-[100%] bg-gray-200 outline-none focus:outline-none p-[2px] sm:p-3 rounded-full"
+          className="w-[100%] bg-gray-200 outline-none focus:outline-none p-3 rounded-full"
           type="text"
           placeholder={`Whats in Your Mind ?`}
         />
@@ -17,17 +21,17 @@ const AddPost = () => {
         onClick={() => {
           AddPost();
         }}
-        className="p-[2px] sm:p-3 bg-[#3B82F6] text-white rounded-full sm:text-sm text-[10px]"
+        className="p-2 sm:p-3 bg-[#3B82F6] text-white rounded-full sm:text-sm  text-sm"
       >
         Add Post
       </button>
 
       <hr className="h-[3px] bg-gray-300 border-none rounded-full" />
 
-      <div className="Bottom flex justify-between p-1 sm:text-sm text-[10px] text-gray-500">
+      <div className="Bottom flex justify-between p-1 sm:text-sm text-sm text-gray-500">
         <div className="A flex items-center cursor-pointer">
           <img
-            className="hover:animate-pulse sm:h-[30px] h-[15px]"
+            className="hover:animate-pulse sm:h-[30px] h-[25px]"
             src="/Emoji/Facebook.png"
             alt=""
           />
@@ -36,7 +40,7 @@ const AddPost = () => {
 
         <div className="flex">
           <img
-            className="hover:animate-spin sm:h-[30px] h-[15px]"
+            className="hover:animate-spin sm:h-[30px] h-[25px]"
             src="/Emoji/Photos.png"
             alt=""
           />
@@ -46,7 +50,7 @@ const AddPost = () => {
 
         <div className="C flex items-center cursor-pointer">
           <img
-            className="hover:animate-bounce sm:h-[30px] h-[15px]"
+            className="hover:animate-bounce sm:h-[30px] h-[25px]"
             src="/Emoji/Smile.png"
             alt=""
           />
--- Client/Components/MainPage/MainPage.js
+++ Client/Components/MainPage/MainPage.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import AddPost from './AddPost'
-import Stories from './Stories'
+import React from "react";
+import AddPost from "./AddPost";
+import Stories from "./Stories";
 const MainPage = () => {
   return (
-    <div className="sm:w-[75%] w-[95%] justify-center flex  mx-auto relative">
-      <div className="bg-white my-5 w-[100%] rounded-sm overflow-x-scroll flex absolute Scroll">
-        <Stories/>
+    <div className="sm:w-[75%] w-[95vw] justify-center flex flex-col mx-auto relative">
+      <div className="bg-white my-5 w-[100%] rounded-sm overflow-x-scroll flex  Scroll">
+        <Stories />
+      </div>
+
+      <div className="w-[100%]">
+        <AddPost />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
